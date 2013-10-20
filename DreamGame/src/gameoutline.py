f7d49d569f09230f784e87e5353ceb2c62b61a23
bill auger <mr.j.spam.me@gmail.com>
2013-10-20 03:35:07 -0400

    add secret message

--- DreamGame/src/gameoutline.py
+++ DreamGame/src/gameoutline.py
@@ -1,8 +1,17 @@
 #!/usr/local/bin/python
 
+mask           = ["this" , "is" , "an" , "ex" , "parrot"]
+secret_message = "tthisiis anfexoparrot thisgisnaniexmparrotathiseisrandex parrotethismisiantex parrotfthisois antexoparrotlthis isaan extparrotnthiseispansex parrotethisvisaanhex parrottthissisuanmex parrotrthisoishantexuparrotathis iseanhextparrot this-is ansexkparrotcthisoisran exeparrotmthisaisgan exsparrotithishistan exwparrotothiswis"
+
+
+def decypher(secret_message):
+  for word in mask:
+    secret_message = secret_message.replace(word , "")
+  return secret_message[::-1]
+
 
 def main():
-    pass
+  print decypher(secret_message)
 
 if __name__ == '__main__':
-    main()
+  main()
